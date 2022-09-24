ed8bbb7ea08ad564dc0036bd552f01d467918e62
MichaelMuz <20muzm01@gmail.com>
2022-09-24 18:50:50 -0400

    conflict?

--- Grid.py
+++ Grid.py
@@ -1,15 +1,32 @@
 from Node import Node
 
 class Grid:
-    def __init__(self, x_size, y_size):
-        self.x_size = x_size
-        self.y_size = y_size
-        self.terrain = []
-        for outside_array in range(y_size):
-            temp = []
-            for inner_array in range(x_size):
-                temp.append(Node(inner_array, outside_array))
-            self.terrain.append(temp)
+    #def __init__(self, x_size, y_size):
+    #    self.x_size = x_size
+    #    self.y_size = y_size
+    #    self.terrain = []
+    #    for outside_array in range(y_size):
+    #        temp = []
+    #        for inner_array in range(x_size):
+    #            temp.append(Node(inner_array, outside_array))
+    #        self.terrain.append(temp)
+    def __init__(self, mapFile):
+        with open(mapFile) as f:
+            cols,rows = list(map(int, f.readline().strip().split()))
+
+            self.obstacles = []
+
+            self.x_size = cols
+            self.y_size = rows
+            
+
+            lines = f.readlines()
+            for line in lines:
+                col,row,val = list(map(int, line.strip().split()))
+                if val == 1:
+                    self.obstacles.append((row,col))
+            
+            
 
     def get_neighbor(current_node: Node):
         neighbors = []
@@ -23,5 +40,4 @@ class Grid:
         if(y < 0 or y > self.y_size):
             return False
 
-    
-
+   
--- driver.py
+++ driver.py
@@ -1,11 +1,8 @@
 from Grid import Grid
-
-grid = Grid(5, 3)
-for x in grid.terrain:
-    for y in x:
-
-        print(y.get_f_value(), end =" ")
-    print("")
+from LosTest import loadMap
+if __name__ == "__main__" :
+    loadMap("map1.txt")
+    
 
 
 
