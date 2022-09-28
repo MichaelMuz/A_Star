557f618b448885d8a9a5c5d2eef425cbc3d46ffe
Luke Letourneau <Bluestackthingie@gmail.com>
2022-09-28 19:16:14 -0400

    update driver

--- driver.py
+++ driver.py
@@ -1,8 +1,15 @@
+import sys
+
+from A_star import A_star
 from Grid import Grid
-from LosTest import load_map
+from NodeGrid import load_map as load_map_gui, trace
+from NodeGrid import run_app
+
 if __name__ == "__main__" :
-    load_map("map1.txt")
-    
-
-
-
+    filename = sys.argv[1]
+    grid = Grid(filename)
+    app, gui_grid = load_map_gui(filename)
+    astar = A_star(grid)
+    astar.run_algorithm()
+    trace(grid, gui_grid)
+    run_app(app)
