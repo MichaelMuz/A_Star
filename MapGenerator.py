aa23550fe9f968100c2883ffbf648e0bd88d5376
CyrusTheVirus <cyrusmajd2001@gmail.com>
2022-10-16 20:25:42 -0400

    map generator does stuff

--- MapGenerator.py
+++ MapGenerator.py
@@ -3,28 +3,117 @@ import random
 
 # Generates maps
 
+def random_end(matrix, numberOfZeroesEnd):
+    ## Select a random start
+    tmp = numberOfZeroesEnd
+    startCord = [len(matrix)-1,-1]
+    for i in range(0, len(matrix[0]), 1):
+
+        # end loop if startCoord defined
+        if (startCord[1] != -1):
+            break
+
+        # take your chances
+        value = random.random()
+        if (matrix[len(matrix)-1][i] == 0):
+            tmp = tmp - 1
+            if (value <= 1 - (float(numberOfZeroesEnd / 10**(len(str(numberOfZeroesEnd)))))):
+                # print(value, float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart)))))
+                startCord[1] = i
+
+        if (tmp == 0 and matrix[len(matrix)-1][i] == 0):
+            # print("forced", value, float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart)))))
+            startCord[1] = i
+
+        # print(matrix[0][i], tmp)
+
+    # print(startCord)
+    return startCord
+
+def random_start(matrix, numberOfZeroesStart):
+    ## Select a random start
+    tmp = numberOfZeroesStart
+    startCord = [0,-1]
+    for i in range(0, len(matrix[0]), 1):
+
+        # end loop if startCoord defined
+        if (startCord[1] != -1):
+            break
+
+        # take your chances
+        value = random.random()
+        if (matrix[0][i] == 0):
+            tmp = tmp - 1
+            if (value <= 1 - (float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart)))))):
+                # print(value, float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart)))))
+                startCord[1] = i
+
+        if (tmp == 0 and matrix[0][i] == 0):
+            # print("forced", value, float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart)))))
+            startCord[1] = i
+
+        # print(matrix[0][i], tmp)
+
+    # print(startCord)
+    return startCord
+
 def main(args):
-    print(args.Dimensions)
-    print(args.BlockRate)
+    # print(args.Dimensions)
+    # print(args.BlockRate)
+    numberOfZeroesStart = 0
+    numberOfZeroesEnd = 0
 
+    ## Generate empty matrix
     rows, cols = (5, 5)
-    matrix = [[0 for i in range(int(args.Dimensions[1]) + 1)] for j in range(int(args.Dimensions[0]) + 1)]
+    matrix = [[0 for i in range(int(args.Dimensions[1]))] for j in range(int(args.Dimensions[0]) + 1)]
 
-    for i in range(0, len(matrix), 1):
-        print(matrix[i])
+    # ## Print empty matrix
+    # for i in range(0, len(matrix), 1):
+    #     print(matrix[i])
 
+    ## Randomly populate empty matrix
     for i in range(0, len(matrix), 1):
         for j in range(0, len(matrix[i]), 1):
             value = random.random()
             if value < float(args.BlockRate[0]):
-                print("ITS A BLOCK:", value)
+                # print("BLOCK:", value)
                 matrix[i][j] = 1
-            else:
-                print("NOT A BLOCK:", value)
-        print("NEW THING")
+            # else:
+                # print("EMPTY:", value)
+        # print("Next Row.")
 
-    for i in range(0, len(matrix), 1):
-        print(matrix[i])
+    # ## Print populated matrix
+    # for i in range(0, len(matrix), 1):
+    #     print(matrix[i])
+
+    ## Find number of zeroes for start and goal
+    for i in range(0, len(matrix[0]), 1):
+        # print(matrix[0][i])
+        if (matrix[0][i]) == 0:
+            numberOfZeroesStart += 1
+        if (matrix[len(matrix)-1][i]) == 0:
+            numberOfZeroesEnd += 1
+
+    # print("====", numberOfZeroesStart, len(str(numberOfZeroesStart)), 10**(len(str(numberOfZeroesStart))))
+    # print("====", numberOfZeroesEnd, len(str(numberOfZeroesEnd)), 10**(len(str(numberOfZeroesEnd))))
+
+    print(args.Dimensions[0], args.Dimensions[1])
+    print(random_start(matrix, numberOfZeroesStart)[0], random_start(matrix, numberOfZeroesStart)[1])
+    print(random_end(matrix, numberOfZeroesEnd)[0], random_end(matrix, numberOfZeroesEnd)[1])
+
+    # print("START: ", random_start(matrix, numberOfZeroesStart))
+    # print("END: ", random_end(matrix, numberOfZeroesEnd))
+
+    ## Print populated matrix
+    rez = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+    for i in range(len(rez)):
+        for j in range(len(rez[i])):
+            print(i+1, j+1, rez[i][j])
+
+    # print("CHANCE: ", 1 - (float(numberOfZeroesStart / 10**(len(str(numberOfZeroesStart))))))
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generates random maps.')
